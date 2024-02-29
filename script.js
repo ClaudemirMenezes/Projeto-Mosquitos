6534493561d8ea77a5913a6f741cb5236bf98357
ClaudemirMenezes <claudemir0088@gmail.com>
2024-02-28 23:48:17 -0300

    Foi criada um pagína inicial para teste de responsividade do Projeto Mosquitos

---
+++ script.js
@@ -0,0 +1,8 @@
+const header = document.querySelector("header");
+const menuBtn = document.querySelector("#menu-btn");
+const closeMenuBtn = document.querySelector("#close-munu.btn");
+
+
+munuBtn.addEventListener("click", () =>{
+    header.classList.toggle("show-mobile-menu");
+});
